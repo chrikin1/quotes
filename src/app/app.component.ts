077b7832be1b6574f2aa44dbcb6e2911577120f7
chrikin1 <chrisianbartoo@gmail.com>
2018-08-10 16:48:33 +0300

    fixed errors and further development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
+import {Quote} from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotes';
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes[
+    new Quote(1,'Your dreams are valid'),
+  ]
 
   constructor() { }
 
