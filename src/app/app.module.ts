ef6a7e772cfe067f57aa406e602bff9197922454
chrikin1 <chrisianbartoo@gmail.com>
2018-08-10 15:16:23 +0300

    debugged and modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteComponent } from './quote/quote.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteDetailsComponent,
     QuoteFormComponent,
-    QuoteComponent
+    QuoteComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule
