12cf926ba23828284ba081a4d46c1d8f9aa61075
chrikin1 <chrisianbartoo@gmail.com>
2018-08-12 23:42:09 +0300

    modify quote form component

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote, QuoteList } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  quote = new Quote("", "", "");
+
+  onSubmit() {
+    QuoteList.push(this.quote);
+    this.quote = new Quote("", "", "");
+  }
 
   constructor() { }
 
