ca25d2e36aa92f02d2d4543089178475595f473d
chrikin1 <chrisianbartoo@gmail.com>
2018-08-14 15:16:20 +0300

    worked on diployment issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteFormComponent,
-    QuoteComponent
+    StrikethroughDirective,
+    QuoteComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -7,7 +7,7 @@ import {Quote} from '../quote'
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
-export class GoalDetailsComponent implements OnInit {
+export class QuoteDetailsComponent implements OnInit {
 
   @Input() quote:Quote;
   @Output() isComplete= new EventEmitter<boolean>();
